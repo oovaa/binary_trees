f25f3eea707e626863e0745b605f205133d9a004
oovaa <omarvx211@gmail.com>
2024-01-29 10:07:54 +0200

    task 2  at 29-10

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = binary_tree_node(parent, value);
+	if (!new_node)
+		return (NULL);
+
+	if (parent->right)
+		new_node->right = parent->right;
+
+	parent->right = new_node;
+
+	return (new_node);
+}
