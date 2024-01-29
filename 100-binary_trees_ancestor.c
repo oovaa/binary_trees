eaa244414be5aa922dfe91d85e24c295b8a5f7b8
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-01-29 23:05:52 +0300

    My brain is dead (advance)

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - finds the lowest common ancestor of two nodes
+ * @first: ptr to first node
+ * @second: ptr to second node
+ * Return: ptr to lowest common ancestor or NULL
+ */
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+{
+    if (first == NULL || second == NULL)
+        return (NULL);
+}
