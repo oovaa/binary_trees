df27c2e326532e50ef94cba391da714cbcacc6e5
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-01-30 09:55:26 +0300

    T2 fix 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -21,6 +21,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	{
 		new_node->right = parent->right;
 		parent->right->parent = new_node;
+		parent->right = NULL;
 	}
 
 	parent->right = new_node;
