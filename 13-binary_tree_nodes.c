8657a56062820f4b6ea082e0874cb3ae859c3799
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-01-30 11:26:21 +0300

    Back to square 1

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,10 +9,13 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	if (!tree)
+		return (0);
+
 	size_t left_nodes = binary_tree_nodes(tree->left);
 	size_t right_nodes = binary_tree_nodes(tree->right);
 
-	if (tree != NULL && (tree->left != NULL || tree->right != NULL))
+	if (tree->left || tree->right)
 		return (1 + left_nodes + right_nodes);
 
 	return (0);
