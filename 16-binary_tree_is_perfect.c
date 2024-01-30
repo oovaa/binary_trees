df6bdafd949b44d3d8cb6b4e645429ce476647b0
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-01-30 18:53:57 +0300

    16 try again

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -5,16 +5,18 @@
  * binary_tree_is_perfect - checks if a binary tree is perfect
  * @tree: Pointer to the root node of the tree
  *
- * Return: if tree is NULL, return 0
+ * Return: 1 if perfect, 0 otherwise
  */
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return 1;
+	int left_height, right_height;
 
-	int left_height = binary_tree_node_height(tree->left, 0);
-	int right_height = binary_tree_node_height(tree->right, 0);
+	if (tree == NULL)
+		return 0;
+
+	left_height = binary_tree_node_height(tree->left, 0);
+	right_height = binary_tree_node_height(tree->right, 0);
 
 	if (left_height == right_height && binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right))
 		return 1;
@@ -26,16 +28,18 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
  * binary_tree_node_height - calculate height of subtree rooted at given node.
  * @node: pointer to the root node of the current subtree
  * @H: current height of subtree
- * Return: Height
+ * Return: Height of the subtree
  */
 
 int binary_tree_node_height(const binary_tree_t *node, int H)
 {
+	int left_height, right_height;
+
 	if (node == NULL)
 		return H;
 
-	int left_height = binary_tree_node_height(node->left, H + 1);
-	int right_height = binary_tree_node_height(node->right, H + 1);
+	left_height = binary_tree_node_height(node->left, H + 1);
+	right_height = binary_tree_node_height(node->right, H + 1);
 
 	return (left_height > right_height ? left_height : right_height);
 }
