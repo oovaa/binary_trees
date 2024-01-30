53472b56e0acbc9ff76f80ab373d210e5b363123
oovaa <omarvx211@gmail.com>
2024-01-30 11:05:54 +0200

    3rd try on 13 at 30-11

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -11,14 +11,14 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t num = 0;
 
-	if (!tree)
-		return (num);
+	if (tree)
+	{
+		num += binary_tree_nodes(tree->left);
+		num += binary_tree_nodes(tree->right);
 
-	size_t left_nodes = binary_tree_nodes(tree->left);
-	size_t right_nodes = binary_tree_nodes(tree->right);
-
-	if (tree->left || tree->right)
-		return (1 + left_nodes + right_nodes);
+		if (tree->left || tree->right)
+			num += 1;
+	}
 
 	return (num);
 }
