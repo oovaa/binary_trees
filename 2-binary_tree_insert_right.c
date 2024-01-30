3ce2c13c49201eb7a82274d56329ab6582bc296e
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-01-30 09:51:36 +0300

    T2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,6 +9,9 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+	if (parent == NULL)
+		return (NULL);
+
 	binary_tree_t *new_node = binary_tree_node(parent, value);
 
 	if (!new_node)
