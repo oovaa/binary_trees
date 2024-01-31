c2a16ba3bdd3ad4014ee8f92faa9da30bb756222
oovaa <omarvx211@gmail.com>
2024-01-31 11:03:14 +0200

    task 103 with a bug at 31-11

---
+++ 103-binary_tree_rotate_left.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_left - function that performs a left-rotation on a BT
+ * @tree: pointer to the root node of the tree to rotate
+ * Return: a pointer to the new root node of the tree once rotated
+ */
+
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+	/* we are given the root we should replace it with it's right kid*/
+	if (!tree | !tree->right)
+		return (NULL);
+
+	binary_tree_t *lc = tree->right, *the_parent = tree->parent;
+
+	tree->right = lc->left;
+	if (tree->right)
+		tree->right->parent = tree;
+
+	lc->left = tree;
+
+	if (the_parent->left == tree)
+		the_parent->left = lc;
+	else
+		the_parent->right = lc;
+
+	tree->parent = lc;
+
+	return (NULL);
+}
