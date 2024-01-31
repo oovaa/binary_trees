0e756c6a278a2c555037d7cacdc1b3fb8bced89a
oovaa <omarvx211@gmail.com>
2024-01-31 18:44:56 +0200

    task 19 done at 31-18

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,29 +1,5 @@
 #include "binary_trees.h"
 
-#include "binary_trees.h"
-
-/**
- * temp_depth - Measures the depth of a binary tree
- * @tree: Pointer to the root node of the tree
- *
- * Return: if tree is NULL, return 0
- */
-
-size_t temp_depth(const binary_tree_t *tree)
-{
-	size_t dep = 0;
-
-	if (tree == NULL)
-		return (0);
-
-	for (; tree->parent != NULL; tree = tree->parent)
-	{
-		dep++;
-	}
-
-	return (dep);
-}
-
 /**
  * binary_trees_ancestor - finds the lowest common ancestor of two nodes
  * @first: ptr to first node
@@ -31,10 +7,29 @@ size_t temp_depth(const binary_tree_t *tree)
  * Return: ptr to lowest common ancestor or NULL
  */
 
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *
+										 first,
+									 const binary_tree_t *second)
 {
+	binary_tree_t *fa, *sa; /* first ancestor, second ancestor */
+
 	if (first == NULL || second == NULL)
 		return (NULL);
+
+	fa = (binary_tree_t *)first;
+
+	while (fa)
+	{
+		sa = (binary_tree_t *)second;
+		while (sa)
+		{
+			if (fa == sa)
+				return (fa);
+
+			sa = sa->parent;
+		}
+		fa = fa->parent;
+	}
 
 	return (NULL);
 }
