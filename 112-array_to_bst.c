f8e1298769ecf94e7c7784ddde701fa15fb31960
EsraaGD <isra.hatim2@gmail.com>
2024-02-01 09:33:53 -0800

    26

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -8,16 +8,16 @@
 
 bst_t *array_to_bst(int *array, size_t size)
 {
-    size_t i;
-    bst_t *root = NULL;
+	size_t i;
+	bst_t *root = NULL;
 
-    for (i = 0; i < size; i++)
-    {
-        if (bst_insert(&root, array[i]) == NULL)
-        {
-            free(root);
-            return (NULL);
-        }
-    }
-    return (root);
+	for (i = 0; i < size; i++)
+	{
+		if (bst_insert(&root, array[i]) == NULL)
+		{
+			free(root);
+			return (NULL);
+		}
+	}
+	return (root);
 }
