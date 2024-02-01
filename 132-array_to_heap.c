809f216ee5bd36231cf5da5d82af51cc624294b2
oovaa <omarvx211@gmail.com>
2024-02-01 20:38:02 +0200

    task 132 array to heap small fix at 01-20

--- 132-array_to_heap.c
+++ 132-array_to_heap.c
@@ -8,24 +8,14 @@
  *
  * Return: Pointer to the root node of the Max Binary Heap.
  */
+
 heap_t *array_to_heap(int *array, size_t size)
 {
-	/* Allocate space for the root pointer */
 	heap_t **root = malloc(sizeof(heap_t *));
 
-	/* Check if memory allocation was successful */
-	if (!root)
-		return (NULL);
-
-	/* Initialize root pointer to NULL */
-	*root = NULL;
-
-	/* Insert each element from the array into the Max Heap */
 	for (size_t i = 0; i < size; i++)
 	{
 		heap_insert(root, array[i]);
 	}
-
-	/* Return the root of the Max Heap */
 	return (*root);
 }
