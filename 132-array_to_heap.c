e6b3dac20857c3a103a41d041fe411911666bb83
oovaa <omarvx211@gmail.com>
2024-02-01 21:16:11 +0200

    task 132 back to .1 at 01-21

--- 132-array_to_heap.c
+++ 132-array_to_heap.c
@@ -13,6 +13,9 @@ heap_t *array_to_heap(int *array, size_t size)
 	heap_t *root;
 	size_t i;
 
+	if (!array || size == 0)
+		return (NULL);
+
 	root = binary_tree_node(NULL, array[0]);
 
 	if (!root)
